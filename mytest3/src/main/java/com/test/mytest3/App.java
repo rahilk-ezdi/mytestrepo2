6ae193e04edddc47fe616362062dc0133b1efbc1
rahilk-ezdi <rahil.k@ezdi.us>
2016-09-30 12:04:03 +0530

    commit

---
+++ mytest3/src/main/java/com/test/mytest3/App.java
@@ -0,0 +1,13 @@
+package com.test.mytest3;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
